454bafaa9e5846deedceeed74a4028b7852e015f
iulfw <luisfernandowilliam68@gmail.com>
2024-05-08 21:09:36 +0700

    Code Cleanup

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-jQuery(document).ready(function($) {
-  "use strict";
+// Navigation
 
-  $('#reviews').owlCarousel({
-      loop: true,
-      center: true,
-      items: 3,
-      margin: 0,
-      autoplay: true,
-      dots: true,
-      autoplayTimeout: 5000,
-      smartSpeed: 750,
-      responsive: {
-        0: {
-          items: 1
-        },
-        768: {
-          items: 2
-        },
-        1170: {
-          items: 3
+function checkScroll() {
+    var startY = $('nav').height() * 2;
+
+    if($(window).scrollTop() > startY) {
+        $('nav').addClass("scroll");
+    } else{
+        $('nav').removeClass("scroll");
+    }
+}
+
+if($('nav').length > 0) {
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
+
+
+// Reviews
+
+jQuery(document).ready(function($) {
+    "use strict";
+
+    $('#reviews').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 750,
+        responsive: {
+            0: {
+            items: 1
+            },
+            768: {
+            items: 2
+            },
+            1170: {
+            items: 3
+            }
         }
-      }
-  });
+    });
 });

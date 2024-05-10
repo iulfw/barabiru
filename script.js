5809493bd49020ad3b620a443d8e8f6d7fcaf13a
iulfw <luisfernandowilliam68@gmail.com>
2024-05-11 02:15:34 +0700

    Responsive Menu & About Page

--- script.js
+++ script.js
@@ -44,3 +44,25 @@ jQuery(document).ready(function($) {
         }
     });
 });
+
+
+// Menu
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+    function filterFunction() {
+        const input = document.getElementById("myInput");
+        const filter = input.value.toUpperCase();
+        const div = document.getElementById("myDropdown");
+        const a = div.getElementsByTagName("a");
+        for (let i = 0; i < a.length; i++) {
+            txtValue = a[i].textContent || a[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                a[i].style.display = "";
+            } else {
+                a[i].style.display = "none";
+        }
+    }
+}
